4d70cfb030db5611692295fd3a795883f48ee890
vsemi <horace.li@vsemi.io>
2019-11-26 21:59:23 -0500

    adding comments to CameraInfo

--- driver/include/camera_info.h
+++ driver/include/camera_info.h
@@ -45,28 +45,70 @@ struct RegionOfInterest_
     uint8_t do_rectify;
 };
 
+
+/**
+* CameraInfo
+*/
 struct CameraInfo {
 
-	   typedef uint32_t _height_type;
-	  _height_type height;
+	typedef uint32_t _height_type;
+	_height_type height;
 
-	   typedef uint32_t _width_type;
-	  _width_type width;
+	typedef uint32_t _width_type;
+	_width_type width;
 
-	   typedef std::vector<double>  _D_type;
-	  _D_type D;
+	/**
+	* The distortion coefficient vector: (k1, k2, p1, p2, k3, k4, k5, k6), 
+	*where k1 to k6 are for radial distortion, and the ps are for tangential distortion.
+	* 
+	* Example: -0.397059, 0.143377, 0, 0, 0, 0, 0, 0
+	* 
+	*/
+	typedef std::vector<double>  _D_type;
+	_D_type D;
 
-	   typedef boost::array<double, 9>  _K_type;
-	  _K_type K;
+	/**
+	* Intrinsic camera matrix for the raw (distorted) images.
+	*     [fx  0 cx]
+	* K = [ 0 fy cy]
+	*     [ 0  0  1]
+	* Projects 3D points in the camera coordinate frame to 2D pixel
+	* coordinates using the focal lengths (fx, fy) and principal point
+	* (cx, cy)
+	* 
+	* Example:
+	* 
+	* 181.296 0         80
+	* 0       181.079   30
+	* 0       0         1
+	* 
+	*/
+	typedef boost::array<double, 9>  _K_type;
+	_K_type K;
 
-	   typedef boost::array<double, 9>  _R_type;
-	  _R_type R;
+	/**
+	* Rectification matrix (stereo cameras only)
+	* A rotation matrix aligning the camera coordinate system to the ideal
+	* stereo image plane so that epipolar lines in both stereo images are
+	* parallel.
+	*/
+	typedef boost::array<double, 9>  _R_type;
+	_R_type R;
 
-	   typedef boost::array<double, 12>  _P_type;
-	  _P_type P;
+	/**
+	* Projection/camera matrix
+	*     [fx'  0  cx' Tx]
+	* P = [ 0  fy' cy' Ty]
+	*     [ 0   0   1   0]
+	*/
+	typedef boost::array<double, 12>  _P_type;
+	_P_type P;
 
-      typedef RegionOfInterest_ _roi_type;
-      _roi_type roi;
+	/**
+	* Region of interest (subwindow of full camera resolution)
+	*/
+	typedef RegionOfInterest_ _roi_type;
+	_roi_type roi;
 };
 
 #endif /* TOF_CAMERA_INFO_H_ */
