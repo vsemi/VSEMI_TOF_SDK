511e226bcb980dea3b085ee74f986fe9b368f6f4
vsemi <horace.li@vsemi.io>
2020-10-28 02:53:54 +0800

    remove OpenCV version requirement

---
+++ samples/sample3/build/CMakeFiles/vsemi_tof_cam.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vsemi_tof_cam.dir/src/main.cpp.o"
+  "vsemi_tof_cam.pdb"
+  "vsemi_tof_cam"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vsemi_tof_cam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
